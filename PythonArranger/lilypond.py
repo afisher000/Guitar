6d12dc193e576bc891e71ffd6ff82689fc7e0910
afisher000 <43121839+afisher000@users.noreply.github.com>
2022-07-09 10:54:47 -0700

    updates

---
+++ PythonArranger/lilypond.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  3 14:45:32 2022
+
+@author: afish
+"""
+
+import os
+
+os.system('lilypond tab.ly')
---
+++ PythonArranger/tab.ly
@@ -0,0 +1,5 @@
+\version "2.22.2"
+\new TabStaff \relative {
+  a,8 a' <c e> a
+  d,8 a' <d f> a
+}
